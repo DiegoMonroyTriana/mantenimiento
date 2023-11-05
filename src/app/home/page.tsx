01d03e238c813fcf2930177c823cf5476a0ec704
Diego Monroy <46665705+DiegoMonroyTriana@users.noreply.github.com>
2023-11-05 00:10:06 -0600

    Delete src/app directory

--- src/app/home/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomePage () {
-  return (
-    <main className=' w-full h-full flex justify-center items-center'>
-      <h1 className='text-4xl'>Home</h1>
-    </main>
-  )
-}
