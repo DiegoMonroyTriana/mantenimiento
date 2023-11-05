75bb4663b5022e9166244f8dd3cebaff825b96c9
Diego Monroy <46665705+DiegoMonroyTriana@users.noreply.github.com>
2023-11-05 00:08:50 -0600

    Delete src/components directory

--- src/components/auth-button-server.tsx
+++
@@ -1,8 +0,0 @@
-import { AuthButton } from '@/components/auth-button-client'
-import supabaseServer from '@/utils/supabaseServer'
-
-export async function AuthButtonServer () {
-  const { data: { session } } = await supabaseServer().auth.getSession()
-
-  return <AuthButton session={session} />
-}
