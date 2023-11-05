3f3a8acd5655598a7969941e158da5a7a658a670
Diego Monroy <46665705+DiegoMonroyTriana@users.noreply.github.com>
2023-11-05 00:11:40 -0600

    Update useMachines.ts

--- src/hooks/useMachines.ts
+++ src/hooks/useMachines.ts
@@ -1,25 +1,71 @@
+import { getDescriptions, getMachines, getMaintenance, getTypes } from '@/services/services'
 import useStore from '@/store/useStore'
-import { type FailureDescriptionType, type FailureType, type MachineType } from '@/types/tables'
+import { type UserType } from '@/types/tables'
 
-const useMachines = ({
-  m,
-  f,
-  fd
-}: {
-  m: MachineType[]
-  f: FailureType[]
-  fd: FailureDescriptionType[]
-}) => {
-  const { setMachines, setFailureDescriptions, setFailureTypes, machines, failureTypes, failureDescriptions } = useStore()
+const useMachines = () => {
+  const {
+    setMachines,
+    setFailures,
+    setFailureTypes,
+    failures,
+    machines,
+    failureTypes,
+    user,
+    allMachines,
+    setAllMachines,
+    setUser: setStoreUser,
+    maintenances,
+    setMaintenances
+  } = useStore()
 
-  setMachines(m)
-  setFailureTypes(f)
-  setFailureDescriptions(fd)
+  async function getMachinesData () {
+    const data = await getMachines()
+    setMachines(data)
+  }
+
+  async function getFailuresData () {
+    const data = await getDescriptions()
+    setFailures(data)
+  }
+
+  async function getFailureTypesData () {
+    const data = await getTypes()
+    setFailureTypes(data)
+  }
+
+  async function setUser (user: UserType) {
+    setStoreUser(user)
+  }
+
+  async function getMaintenancesData () {
+    const { body } = await getMaintenance()
+    setMaintenances(body)
+  }
+
+  async function getAllMachinesData () {
+    const data = await getMachines(true)
+    setAllMachines(data)
+  }
+
+  async function getMaintenanceById (id: string) {
+    const { body } = await getMaintenance(id)
+    return body
+  }
 
   return {
     machines,
+    failures,
     failureTypes,
-    failureDescriptions
+    user,
+    maintenances,
+    allMachines,
+    getMaintenanceById,
+    getAllMachinesData,
+    getMaintenancesData,
+    setUser,
+    getMachinesData,
+    getFailuresData,
+    getFailureTypesData
   }
 }
 
