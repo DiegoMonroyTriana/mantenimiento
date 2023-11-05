907570437fad2b57bda4b82aaf166d8d96edcd0c
Diego Monroy <46665705+DiegoMonroyTriana@users.noreply.github.com>
2023-11-05 00:11:13 -0600

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,3 +3,5 @@ export const REDIRECT_URL = process.env.NEXT_PUBLIC_REDIRECT_URL
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
 
 export const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY
+
+export const PREVENTIVE_ID = '1abe362f-2f7b-4011-a30f-03f53a110eb6'
