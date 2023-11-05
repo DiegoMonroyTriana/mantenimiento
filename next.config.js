e80938b3a4cdfd573a87b142e6eb5859aa378f71
Diego Monroy <46665705+DiegoMonroyTriana@users.noreply.github.com>
2023-11-05 00:13:59 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["@nivo"],
   "experimental": {
     "serverActions": true
-  }
+  },
 }
 
 module.exports = nextConfig
