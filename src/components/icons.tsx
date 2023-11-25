100f1a3d175e646288d74fb054f7d7862c2535b4
Diego Monroy <46665705+DiegoMonroyTriana@users.noreply.github.com>
2023-11-25 15:37:07 -0600

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,7 +1,7 @@
 export const GoogleIcon = ({
-  className = 'w-4 h-4 mr-2'
+  className = 'w-4 h-4 mr-2',
 }: {
-  className?: string
+  className?: string;
 }) => (
   <svg
     className={className}
@@ -14,7 +14,7 @@ export const GoogleIcon = ({
       clipRule="evenodd"
     />
   </svg>
-)
+);
 
 export const MenuIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
   <svg
@@ -30,12 +30,12 @@ export const MenuIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
     <path d="M7 12h13"></path>
     <path d="M10 18h10"></path>
   </svg>
-)
+);
 
 export const LogoutIcon = ({
-  className = 'w-4 h-4'
+  className = 'w-4 h-4',
 }: {
-  className?: string
+  className?: string;
 }) => (
   <svg
     className={className}
@@ -50,7 +50,7 @@ export const LogoutIcon = ({
     <path d="M15 12h-12l3 -3"></path>
     <path d="M6 15l-3 -3"></path>
   </svg>
-)
+);
 
 export const HomeIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
   <svg
@@ -66,7 +66,7 @@ export const HomeIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
     <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
     <path d="M10 12h4v4h-4z"></path>
   </svg>
-)
+);
 
 export const NoteIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
   <svg
@@ -83,7 +83,7 @@ export const NoteIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
     <path d="M9 11l6 0"></path>
     <path d="M9 15l4 0"></path>
   </svg>
-)
+);
 
 export const ToolIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
   <svg
@@ -97,75 +97,203 @@ export const ToolIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
     <path d="M7 10h3v-3l-3.5 -3.5a6 6 0 0 1 8 8l6 6a2 2 0 0 1 -3 3l-6 -6a6 6 0 0 1 -8 -8l3.5 3.5"></path>
   </svg>
-)
+);
 
-export const CameraIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
-  <svg className={className} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-   <path d="M15 3a2 2 0 0 1 1.995 1.85l.005 .15a1 1 0 0 0 .883 .993l.117 .007h1a3 3 0 0 1 2.995 2.824l.005 .176v9a3 3 0 0 1 -2.824 2.995l-.176 .005h-14a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-9a3 3 0 0 1 2.824 -2.995l.176 -.005h1a1 1 0 0 0 1 -1a2 2 0 0 1 1.85 -1.995l.15 -.005h6zm-3 7a3 3 0 0 0 -2.985 2.698l-.011 .152l-.004 .15l.004 .15a3 3 0 1 0 2.996 -3.15z" strokeWidth="0" fill="currentColor"></path>
+export const CameraIcon = ({
+  className = 'w-4 h-4',
+}: {
+  className?: string;
+}) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path
+      d="M15 3a2 2 0 0 1 1.995 1.85l.005 .15a1 1 0 0 0 .883 .993l.117 .007h1a3 3 0 0 1 2.995 2.824l.005 .176v9a3 3 0 0 1 -2.824 2.995l-.176 .005h-14a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-9a3 3 0 0 1 2.824 -2.995l.176 -.005h1a1 1 0 0 0 1 -1a2 2 0 0 1 1.85 -1.995l.15 -.005h6zm-3 7a3 3 0 0 0 -2.985 2.698l-.011 .152l-.004 .15l.004 .15a3 3 0 1 0 2.996 -3.15z"
+      strokeWidth="0"
+      fill="currentColor"></path>
   </svg>
-)
+);
 
-export const CloseIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
-  <svg className={className} strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-   <path d="M18 6l-12 12"></path>
-   <path d="M6 6l12 12"></path>
+export const CloseIcon = ({
+  className = 'w-4 h-4',
+}: {
+  className?: string;
+}) => (
+  <svg
+    className={className}
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M18 6l-12 12"></path>
+    <path d="M6 6l12 12"></path>
   </svg>
-)
+);
 
-export const ClockIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
-  <svg className={className} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-   <path d="M20.942 13.021a9 9 0 1 0 -9.407 7.967"></path>
-   <path d="M12 7v5l3 3"></path>
-   <path d="M15 19l2 2l4 -4"></path>
+export const ClockIcon = ({
+  className = 'w-4 h-4',
+}: {
+  className?: string;
+}) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M20.942 13.021a9 9 0 1 0 -9.407 7.967"></path>
+    <path d="M12 7v5l3 3"></path>
+    <path d="M15 19l2 2l4 -4"></path>
   </svg>
-)
+);
 
-export const TractorIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
-  <svg className={className} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-   <path d="M7 15m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
-   <path d="M7 15l0 .01"></path>
-   <path d="M19 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-   <path d="M10.5 17l6.5 0"></path>
-   <path d="M20 15.2v-4.2a1 1 0 0 0 -1 -1h-6l-2 -5h-6v6.5"></path>
-   <path d="M18 5h-1a1 1 0 0 0 -1 1v4"></path>
+export const TractorIcon = ({
+  className = 'w-4 h-4',
+}: {
+  className?: string;
+}) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M7 15m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+    <path d="M7 15l0 .01"></path>
+    <path d="M19 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+    <path d="M10.5 17l6.5 0"></path>
+    <path d="M20 15.2v-4.2a1 1 0 0 0 -1 -1h-6l-2 -5h-6v6.5"></path>
+    <path d="M18 5h-1a1 1 0 0 0 -1 1v4"></path>
   </svg>
-)
+);
 
-export const SearchIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
-  <svg className={className} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-   <circle cx="10" cy="10" r="7"></circle>
-   <line x1="21" y1="21" x2="15" y2="15"></line>
+export const SearchIcon = ({
+  className = 'w-4 h-4',
+}: {
+  className?: string;
+}) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <circle cx="10" cy="10" r="7"></circle>
+    <line x1="21" y1="21" x2="15" y2="15"></line>
   </svg>
-)
+);
 
-export const CheckIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
-  <svg className={className} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-   <path d="M5 12l5 5l10 -10"></path>
+export const CheckIcon = ({
+  className = 'w-4 h-4',
+}: {
+  className?: string;
+}) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M5 12l5 5l10 -10"></path>
   </svg>
-)
+);
 
-export const EngineIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
-  <svg className={className} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-   <rect x="3" y="8" width="18" height="8" rx="1"></rect>
-   <circle cx="7" cy="12" r="1"></circle>
-   <circle cx="17" cy="12" r="1"></circle>
+export const EngineIcon = ({
+  className = 'w-4 h-4',
+}: {
+  className?: string;
+}) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <rect x="3" y="8" width="18" height="8" rx="1"></rect>
+    <circle cx="7" cy="12" r="1"></circle>
+    <circle cx="17" cy="12" r="1"></circle>
   </svg>
-)
+);
 
-export const TrashIcon = ({ className = 'w-4 h-4' }: { className?: string }) => (
-  <svg className={className} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-   <line x1="4" y1="7" x2="20" y2="7"></line>
-   <line x1="10" y1="11" x2="10" y2="17"></line>
-   <line x1="14" y1="11" x2="14" y2="17"></line>
-   <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
-   <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+export const TrashIcon = ({
+  className = 'w-4 h-4',
+}: {
+  className?: string;
+}) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <line x1="4" y1="7" x2="20" y2="7"></line>
+    <line x1="10" y1="11" x2="10" y2="17"></line>
+    <line x1="14" y1="11" x2="14" y2="17"></line>
+    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
   </svg>
-)
+);
+
+export const ArrowUp = ({ className = 'w-4 h-4' }: { className?: string }) => (
+  <svg
+    className={className}
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M3 17l6 -6l4 4l8 -8" />
+    <path d="M14 7l7 0l0 7" />
+  </svg>
+);
+
+export const ArrowDown = ({
+  className = 'w-4 h-4',
+}: {
+  className?: string;
+}) => (
+  <svg
+    className={className}
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M3 7l6 6l4 -4l8 8" />
+    <path d="M21 10l0 7l-7 0" />
+  </svg>
+);
